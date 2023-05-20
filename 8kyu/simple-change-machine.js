fbbf3af8859110637820200e55464cc944da84c9
castthecalmingapple <kyletucci@gmail.com>
2023-05-20 19:20:15 -0400

    Coding challenge solution for 05-20-23

---
+++ 8kyu/simple-change-machine.js
@@ -0,0 +1,24 @@
+// Description:
+
+// Your task is to create a change machine.
+
+// The machine accepts a range of specified coins and notes, it returns change in 20p and 10p coins in the minimum amount of pieces. A 50p for example would return two 20p pieces and one 10p piece. The machine will always try and return change, if you input a 20p for example it will return "10p 10p".
+
+// The machine accepts these coins and notes: £5, £2, £1, 50p, 20p. Any coins and notes which are not accepted by the machine will be returned. If you were to put a £20 note into the machine for example, it would be returned to you and not broken into change.
+
+// This change machine is programmed to accept and distribute strings rather than numbers. The change will be returned as one string with the change separated by single spaces & no commas. The values of the string will be descending.
+
+//My solution
+function changeMe(moneyIn){
+    //returns only 20p and 10p
+    //Accepts only 20p, 50p, 1, 2, 5
+    let change = {
+      '£5':'20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p 20p',
+      '£2':'20p 20p 20p 20p 20p 20p 20p 20p 20p 20p',
+      '£1':'20p 20p 20p 20p 20p',
+      '50p':'20p 20p 10p',
+      '20p':'10p 10p',
+      'Money': 'Money',
+    }
+    return(change[moneyIn] || moneyIn)
+  }
