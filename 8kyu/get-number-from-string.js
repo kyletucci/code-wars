47d2cb23beafbd2575839d084a23ea24d0be496a
castthecalmingapple <kyletucci@gmail.com>
2023-07-29 19:14:56 -0400

    Coding challenge solution for 07-29-23

---
+++ 8kyu/get-number-from-string.js
@@ -0,0 +1,12 @@
+// Description:
+
+// Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
+
+// Function:
+
+// getNumberFromString(s)
+
+//My solution
+function getNumberFromString(s) {
+    return Number(s.match(/[0-9]/gi).join(""))
+  }
