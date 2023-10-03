867563a1159c66bcde3469aaceeeabbbb5db8068
castthecalmingapple <kyletucci@gmail.com>
2023-10-02 22:42:32 -0400

    Pushing coding challenge solution for 10-02-23

---
+++ 8kyu/reversed.sequence.js
@@ -0,0 +1,15 @@
+// Description:
+
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+
+//My solution
+const reverseSeq = n => {
+    let reverseArray = []
+    for(i = n; i > 0; i--){
+      reverseArray.push(i)
+    }
+    return reverseArray
+  }
