c18fdf24fb5fce59a915aca22696d7a47ecb23e1
castthecalmingapple <kyletucci@gmail.com>
2023-10-10 22:32:54 -0400

    Pushing coding challenge solution for 10-10-23

---
+++ 8kyu/calculate-bmi.js
@@ -0,0 +1,29 @@
+// Description:
+
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+
+
+//My solution
+function bmi(weight, height) {
+    let bmiResult = weight / Math.pow(height,2) 
+    if(bmiResult <= 18.5){
+      return "Underweight"
+    }
+    else if(bmiResult <= 25.0){
+      return "Normal"
+    }
+    else if(bmiResult <= 30.0){
+      return "Overweight"
+    }
+    else {
+      return "Obese"
+    }
+  }
