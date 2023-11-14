77014093fb0e76a519450b047859d67581b28daa
castthecalmingapple <kyletucci@gmail.com>
2023-11-14 18:30:42 -0500

    Pushing coding challenge solution for 11-14-23

---
+++ 8kyu/oppsite-number.js
@@ -0,0 +1,16 @@
+// Description:
+
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+
+//My solution
+function opposite(number) {
+    return 0 - number
+  }
